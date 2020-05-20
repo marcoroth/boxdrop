cd31b4f8e076c9d0df0647c363bb872cd57f7dc9
Marco Roth <marco.roth@intergga.ch>
2020-05-21 00:34:31 +0200

    rename app to boxdrop

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "dropbox_clone_production"
+  # config.active_job.queue_name_prefix = "boxdrop_production"
 
   config.action_mailer.perform_caching = false
 
