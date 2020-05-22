21e838ebbf0fb024a9d196e8d057bff4cc3a8af1
Marco Roth <marco.roth@intergga.ch>
2020-05-22 23:32:39 +0200

    identify ActionCable connection by request session id

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,9 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :session_id
+
+    def connect
+      self.session_id = request.session.id
+    end
   end
 end
