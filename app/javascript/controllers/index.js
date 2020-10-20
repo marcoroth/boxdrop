f5d8d587922d6a856275419636a35313d8258f40
Marco Roth <marco.roth@intergga.ch>
2020-10-20 18:36:34 +0200

    add @stimulus/polyfills
    https://stimulusjs.org/handbook/installing#browser-support

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,6 +1,7 @@
 // Load all the controllers within this directory and all subdirectories.
 // Controller files must be named *_controller.js.
 
+import "@stimulus/polyfills"
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
 import StimulusReflex from 'stimulus_reflex'
