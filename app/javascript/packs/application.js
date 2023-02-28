0210ce02e37d8e6e257537dbccd062656cc91f05
Marco Roth <marco.roth@intergga.ch>
2023-02-28 18:35:50 +0100

    Remove `@rails/ujs`

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,6 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
 require("@hotwired/turbo-rails")
 require("@rails/activestorage").start()
 require("channels")
